78232dd14dd4df0bf447aeb90948057d862e6f33
ltostes <lucasrmtostes@gmail.com>
2025-03-12 15:25:13 -0300

    version zero of tostes live

---
+++ README.md
@@ -0,0 +1,1 @@
+# The first ever setup of tostes.live :)
---
+++ backend/server.js
@@ -0,0 +1,17 @@
+// backend/server.js
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Serve static files from React build
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
