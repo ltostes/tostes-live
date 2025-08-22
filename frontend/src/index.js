8708e7e9f3f2505f4c4f81cb85e690eea4c19ad9
ltostes <lucasrmtostes@gmail.com>
2025-08-22 17:58:39 -0300

    board game prototype kickoff

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,12 +6,14 @@ import './index.css';
 
 import Main from './pages/Main/Main';
 import RollerCoasterRama from './pages/RollerCoasterRama';
+import BoardGamePrototype from './pages/BoardGamePrototype/BoardGamePrototype';
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   { path: "/", element: <Main />},
   { path: "/gmtk-gamejam-2025", element: <RollerCoasterRama />},
+  { path: "/boardgameprototype", element: <BoardGamePrototype />},
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
