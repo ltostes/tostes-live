1fe075baed10a31a70538d4adde82ad8ee6c5c44
ltostes <lucasrmtostes@gmail.com>
2025-09-07 10:39:17 -0300

    added card definitions

--- frontend/src/components/Sobrevivencia/constants.js
+++ frontend/src/components/Sobrevivencia/constants.js
@@ -1,3 +1,12 @@
+export const STARTING_LOCATIONS = [
+    { id: 0, row: 1, col: 2, dir: 5 },
+    { id: 1, row: 4, col: 0, dir: 0 },
+    { id: 2, row: 7, col: 1, dir: 1 },
+    { id: 3, row: 0, col: 17, dir: 4 },
+    { id: 4, row: 3, col: 19, dir: 3 },
+    { id: 5, row: 5, col: 20, dir: 3 },
+    { id: 6, row: 8, col: 18, dir: 2 },
+]
 
 // Generated with https://docs.google.com/spreadsheets/d/1_a2cpSjOCRqpUG_rj5EJQTnwpdk0Oef0Wvwgay6lW1U/edit?gid=0#gid=0
 
