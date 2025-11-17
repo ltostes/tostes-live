277bfd346c5a24c774b2e1a0eaf104339e2eac7c
ltostes <lucasrmtostes@gmail.com>
2025-11-17 20:29:38 -0300

    updated main

--- apps/main/src/pages/Main/Main.jsx
+++ apps/main/src/pages/Main/Main.jsx
@@ -13,6 +13,12 @@ function Main() {
       <Link to={"/gmtk-gamejam-2025"}>
         #1 RollerCoaster Rama!
       </Link>
+      <Link to={"https://tostes-live-sobrevivencia.vercel.app/"} target='_blank' rel="noreferrer">
+        #2 Protótipo Sobrevivência na Amazônia
+      </Link>
+      <Link to={"https://emotionalgeographies-interactivity.vercel.app/TOSTESLIVE/presentation"} target='_blank' rel="noreferrer">
+        #3 Interatividade Emotional Geographies
+      </Link>
     </StandardPage>
 }
 
